994fcfa0896b603dac52dba7b9384e9328bf769b
Dwayne Munro <dwaynemunro1845@gmail.com>
2019-02-02 18:00:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,3 +26,43 @@ def won?
   end
   result
 end
+
+def won?
+    result = false
+    WIN_COMBINATIONS.each do |win_combo|
+      subset = [@board[win_combo[0]], @board[win_combo[1]], @board[win_combo[2]]]
+      if subset.all? {|i| i == "X"} || subset.all? {|i| i == "O"}
+        result = subset
+      end
+    end
+    result
+  end
+
+  def full?
+    @board.reject{|i| ["X", "O"].include?(i)}.count == 0 ? true : false
+  end
+
+  def over?
+    draw? || won?
+  end
+
+  def winner
+    x = won?
+    x ? x[0] : nil
+  end
+
+  def play
+    ## Keep going until the game ends
+    until over?
+      turn
+    end
+
+    # ## Tell the user what happened after the game ends
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cats Game!"
+    end
+  end
+
+end

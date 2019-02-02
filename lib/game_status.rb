7ee2bd3e0e5f943f2da4744ed49645ebf53fe559
Dwayne Munro <dwaynemunro1845@gmail.com>
2019-02-02 17:58:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,13 @@ end
 
 # Define your WIN_COMBINATIONS constant
 
-
-def won(board) 
-  
+def won?
+  result = false
+  WIN_COMBINATIONS.each do |win_combo|
+    subset = [@board[win_combo[0]], @board[win_combo[1]], @board[win_combo[2]]]
+    if subset.all? {|i| i == "X"} || subset.all? {|i| i == "O"}
+      result = subset
+    end
+  end
+  result
 end
